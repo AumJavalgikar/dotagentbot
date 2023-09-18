b20295e55d98de2c217a99b74d31ce217757af6f
AumJavalgikar <aumjavalgikar@gmail.com>
2023-09-18 19:24:12 +0530

    remove async_mode=True from get_memory

--- agents/memory/interviewer_summary_memory.py
+++ agents/memory/interviewer_summary_memory.py
@@ -46,7 +46,7 @@ class InterviewerMemory(BaseMemory, BaseModel):
                 self.messages_in_summary.append(conversation)
 
             summarizer = compiler(template=INTERVIEWER_SUMMARIZER_TEMPLATE, llm=llm, stream=False,
-                                  max_questions=self.max_questions, interview_goals=self.interview_goals, async_mode=True)
+                                  max_questions=self.max_questions, interview_goals=self.interview_goals)
             summarized_memory = summarizer(summary=self.current_summary, new_lines=messages_to_text)
             print(f'current summary : {summarizer.get("result")}')
             self.current_summary = extract_text(summarized_memory.text)
