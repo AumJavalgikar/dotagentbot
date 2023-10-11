8d28078764e2641b927b7365d786ffdbb7570863
AumJavalgikar <aumjavalgikar@gmail.com>
2023-10-11 11:40:31 +0530

    calling super() and added hidden blocks

--- agents/dnd_agent/__init__.py
+++ agents/dnd_agent/__init__.py
@@ -35,7 +35,8 @@ class DnDAgent(BaseAgent):
         self.current_area = final_area,
         self.tools = (self.get_all_classes, self.get_all_races, self.get_all_areas)
         self.compiler: Program = initialize_dotagent_client(llm=llm, file_name='dnd', memory=memory, async_mode=True)
-        self.output_key = 'followup'
+        # self.output_key = 'followup'
+        self.return_complete = True
 
     def agent_type(self):
         return "chat"
@@ -49,41 +50,19 @@ class DnDAgent(BaseAgent):
     def get_all_areas(self):
         return '\n'.join([f'{unique_area.name} - {unique_area.description}' for unique_area in self.all_races])
 
-    async def run(self, **kwargs) -> Union[str, Dict[str, Any]]:
-        """Run the agent to generate a response to the user query."""
-
-        _knowledge_variable = self.get_knowledge_variable
-
-        if _knowledge_variable:
-            if kwargs.get(_knowledge_variable):
-                query = kwargs.get(_knowledge_variable)
-                retrieved_knowledge = self.get_knowledge(query)
-                output = self.compiler(RETRIEVED_KNOWLEDGE=retrieved_knowledge, **kwargs, silent=True)
-            else:
-                raise ValueError("knowledge_variable not found in input kwargs")
-        else:
-            output = await self.compiler(dungeon_master_info=self.system_prompt,
-                                         player_name=self.player_name,
-                                         player_class=self.player_class,
-                                         player_race=self.player_race,
-                                         player_attributes=self.player_attributes,
-                                         current_area=f'Area name: {self.current_area}\n'
-                                                      f'Area description {self.current_area}',
-                                         dnd_agent=id(self),
-                                         tool=self.tools,
-                                         tool_func=tool_use,
-                                         **kwargs)
-
-        if self.return_complete:
-            return output
-        print(f'Output of entire query : {output}')
-        _output_key = self.output_key if self.output_key is not None else self.get_output_key(output)
-
-        if output.variables().get(_output_key):
-            return output[_output_key]
-        else:
-            logging.warning("Output key not found in output, so full output returned")
-            return output
+    async def run(self, **kwargs):
+        kwargs = {**{'dungeon_master_info': self.system_prompt,
+                     'player_name': self.player_name,
+                     'player_class': self.player_class,
+                     'player_race':self.player_race,
+                     'player_attributes': self.player_attributes,
+                     'current_area': f'Area name: {self.current_area}\n'
+                                     f'Area description {self.current_area}'},
+                    'dnd_agent' : id(self),
+                    'tool' : self.tools,
+                    'tool_func' : tool_use, **kwargs}
+        output = await super().run(**kwargs)
+        return output
 
 
 def tool_use(var: dict):
