dd6ffd2357f6f48714929b578d86eafd1d9bcab3
AumJavalgikar <aumjavalgikar@gmail.com>
2024-01-05 18:33:58 +0530

    fix bug

--- cogs/cog_nextpy.py
+++ cogs/cog_nextpy.py
@@ -18,7 +18,7 @@ class cog_nextpy(commands.Cog):
     async def query(self, ctx: ApplicationContext, query: str):
         await ctx.defer()
         response: Program = await self.bot.nextpy_client.run(user_text=query)
-        await ctx.respond(response.get('choices').get('message').get('content'))
+        await ctx.respond(response.get('choices')[0].get('message').get('content'))
 
 
 def setup(client):
